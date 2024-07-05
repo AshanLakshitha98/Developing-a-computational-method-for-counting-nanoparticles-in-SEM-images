77e59f7f49b658f51cb4bf5385ff94c37636be40
AshanLakshitha98 <ashan1998@gmail.com>
2024-07-05 23:17:42 +0530

    model training code

---
+++ model_train.py
@@ -0,0 +1,279 @@
+from keras.utils import normalize
+import os
+import cv2
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+from patchify import patchify, unpatchify
+import tifffile as tiff
+from sklearn.preprocessing import MinMaxScaler
+from keras.optimizers import Adam
+
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
+from keras.optimizers import Adam
+from keras.layers import Activation, MaxPool2D, Concatenate
+
+
+def conv_block(input, num_filters):
+    x = Conv2D(num_filters, 3, padding="same")(input)
+    x = BatchNormalization()(x)   #Not in the original network. 
+    x = Activation("relu")(x)
+
+    x = Conv2D(num_filters, 3, padding="same")(x)
+    x = BatchNormalization()(x)  #Not in the original network
+    x = Activation("relu")(x)
+
+    return x
+
+def encoder_block(input, num_filters):
+    x = conv_block(input, num_filters)
+    p = MaxPool2D((2, 2))(x)
+    return x, p   
+
+def decoder_block(input, skip_features, num_filters):
+    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+    x = Concatenate()([x, skip_features])
+    x = conv_block(x, num_filters)
+    return x
+
+#Build Unet using the blocks
+def build_unet(input_shape):
+    inputs = Input(input_shape)
+
+    s1, p1 = encoder_block(inputs, 64)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+    s4, p4 = encoder_block(p3, 512)
+
+    b1 = conv_block(p4, 1024) #Bridge
+
+    d1 = decoder_block(b1, s4, 512)
+    d2 = decoder_block(d1, s3, 256)
+    d3 = decoder_block(d2, s2, 128)
+    d4 = decoder_block(d3, s1, 64)
+
+    outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d4)  #Binary (can be multiclass)
+
+    model = Model(inputs, outputs, name="U-Net")
+    return model
+
+
+# In[3]:
+
+
+import glob,os
+path='Dwn_imgs/TEM_all/Model1/Img/*.*'
+all_img_patches = []
+
+for file in sorted(glob.glob(path)):
+    #img=io.read(file)
+    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+
+    large_image=img
+
+    step=128
+
+    patches_img = patchify(large_image, (step, step), step=step)  #Step=256 for 256 patches means no overlap
+
+
+    for i in range(patches_img.shape[0]):
+        for j in range(patches_img.shape[1]):
+
+            single_patch_img = patches_img[i,j,:,:]
+            single_patch_img = (single_patch_img.astype('float32')) / 255.
+            #scaler = MinMaxScaler()
+            #single_patch_img= scaler.fit_transform(single_patch_img)
+
+            all_img_patches.append(single_patch_img)    
+
+images = np.array(all_img_patches)
+images = np.expand_dims(images, -1)
+
+
+# In[4]:
+
+
+path='Dwn_imgs/TEM_all/Model1/Mask/*.*'
+all_mask_patches = []
+
+for file in sorted(glob.glob(path)):
+    img_mask = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    img_mask = cv2.threshold(img_mask, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    large_mask = img_mask
+
+    patches_mask = patchify(large_mask, (step, step), step=step)  #Step=256 for 256 patches means no overlap
+
+
+    for i in range(patches_mask.shape[0]):
+        for j in range(patches_mask.shape[1]):
+
+            single_patch_mask = patches_mask[i,j,:,:]
+            single_patch_mask = single_patch_mask / 255.
+
+            all_mask_patches.append(single_patch_mask)
+
+masks = np.array(all_mask_patches)
+masks = np.expand_dims(masks, -1)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(images, masks, test_size = 0.25, random_state = 0)
+
+
+import random
+import numpy as np
+image_number = random.randint(0, len(X_train))
+plt.figure(figsize=(12, 6))
+plt.subplot(121)
+plt.imshow(np.reshape(X_train[image_number], (step, step)), cmap='gray')
+plt.subplot(122)
+plt.imshow(np.reshape(y_train[image_number], (step, step)), cmap='gray')
+plt.show()
+
+
+# In[9]:
+
+
+IMG_HEIGHT = images.shape[1]
+IMG_WIDTH  = images.shape[2]
+IMG_CHANNELS = images.shape[3]
+
+
+input_shape = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+
+model = build_unet(input_shape)
+#model.compile(optimizer=Adam(lr = 1e-3), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=1e-3), loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
+
+ 
+seed=24
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+img_data_gen_args = dict(rotation_range=90,
+                     width_shift_range=0.3,
+                     height_shift_range=0.3,
+                     shear_range=0.5,
+                     zoom_range=0.3,
+                     horizontal_flip=True,
+                     vertical_flip=True,
+                     fill_mode='reflect')
+
+mask_data_gen_args = dict(rotation_range=90,
+                     width_shift_range=0.3,
+                     height_shift_range=0.3,
+                     shear_range=0.5,
+                     zoom_range=0.3,
+                     horizontal_flip=True,
+                     vertical_flip=True,
+                     fill_mode='reflect',
+                     preprocessing_function = lambda x: np.where(x>0, 1, 0).astype(x.dtype)) #Binarize the output again. 
+
+image_data_generator = ImageDataGenerator(**img_data_gen_args)
+
+
+batch_size= 8
+
+image_generator = image_data_generator.flow(X_train, seed=seed, batch_size=batch_size)
+valid_img_generator = image_data_generator.flow(X_test, seed=seed, batch_size=batch_size) #Default batch size 32, if not specified here
+
+mask_data_generator = ImageDataGenerator(**mask_data_gen_args)
+mask_generator = mask_data_generator.flow(y_train, seed=seed, batch_size=batch_size)
+valid_mask_generator = mask_data_generator.flow(y_test, seed=seed, batch_size=batch_size)  #Default batch size 32, if not specified here
+
+def my_image_mask_generator(image_generator, mask_generator):
+    train_generator = zip(image_generator, mask_generator)
+    for (img, mask) in train_generator:
+        yield (img, mask)
+
+my_generator = my_image_mask_generator(image_generator, mask_generator)
+
+validation_datagen = my_image_mask_generator(valid_img_generator, valid_mask_generator)
+
+
+x = image_generator.__next__()
+y = mask_generator.__next__()
+for i in range(0,1):
+    image = x[i]
+    mask = y[i]
+    plt.subplot(1,2,1)
+    plt.imshow(image[:,:,0], cmap='gray')
+    plt.subplot(1,2,2)
+    plt.imshow(mask[:,:,0])
+    plt.show()
+
+
+# In[13]:
+
+
+steps_per_epoch = 3*(len(X_train))//batch_size
+
+history = model.fit(my_generator, validation_data=validation_datagen, 
+                    steps_per_epoch=steps_per_epoch, 
+                    validation_steps=steps_per_epoch, epochs=25)
+
+#plot the training and validation accuracy and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+
+# In[15]:
+
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+plt.plot(epochs, acc, 'y', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+
+# In[16]:
+
+
+#IOU
+y_pred=model.predict(X_test)
+y_pred_thresholded = y_pred > 0.5
+
+intersection = np.logical_and(y_test, y_pred_thresholded)
+union = np.logical_or(y_test, y_pred_thresholded)
+iou_score = np.sum(intersection) / np.sum(union)
+print("IoU socre is: ", iou_score)
+
+
+test_img_number = random.randint(0, len(X_test))
+test_img = X_test[test_img_number]
+ground_truth=y_test[test_img_number]
+test_img_norm=test_img[:,:,0][:,:,None]
+test_img_input=np.expand_dims(test_img_norm, 0)
+prediction = (model.predict(test_img_input)[0,:,:,0] > 0.2).astype(np.uint8)
+
+plt.figure(figsize=(16, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_img[:,:,0], cmap='gray')
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(ground_truth[:,:,0], cmap='gray')
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(prediction, cmap='gray')
+
+plt.show()
+
+
+from tensorflow.keras.models import load_model, save_model
+model.save("savedmodel/modelUNET_TEM(Model1).h5")
